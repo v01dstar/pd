15324c508eff6a765222bc1eca303e12f0c81a87
Yang Zhang <yang.zhang@pingcap.com>
2023-07-03 19:59:14 -0700

    unsafe recovery: Fixing learner store being ignored error (#6683)
    ref tikv/pd#6690, ref tikv/pd#6709
    
    Fix learner store/ replica being ignored error in auto detect mode.
    
    Signed-off-by: Yang Zhang <yang.zhang@pingcap.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- server/cluster/unsafe_recovery_controller.go
+++ server/cluster/unsafe_recovery_controller.go
@@ -706,9 +706,6 @@ func (u *unsafeRecoveryController) getFailedPeers(region *metapb.Region) []*meta
 
 	var failedPeers []*metapb.Peer
 	for _, peer := range region.Peers {
-		if peer.Role == metapb.PeerRole_Learner || peer.Role == metapb.PeerRole_DemotingVoter {
-			continue
-		}
 		if u.isFailed(peer) {
 			failedPeers = append(failedPeers, peer)
 		}
